1b47b48aaeb747f542e65a8aa1b5b897ea7c61a9
kallepaas <kalle.paas@voco.ee>
2022-10-29 14:57:14 +0300

    Lesson 9: Components & Props (ei teinud challengeit)

---
+++ components/ProductDisplay.js
@@ -0,0 +1,104 @@
+app.component('product-display', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true,
+        }
+    },
+    template:
+    /*html*/
+    `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <!-- vbind:src võib lühendada :src; coding challenge'ist mitteaktiivne toote tuhmiks -->
+        <img :class="{ 'out-of-stock-img': !inStock }" v-bind:src="image">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <!-- challenge'i lahendus, ei mõelnud ise välja -->
+        <p v-if="onSale">{{ saleMessage }}</p>
+        <!-- Coding challenge'is loodud -->
+        <p v-if="inStock">On saadaval</p>
+        <p v-else>Pole saadaval</p>
+        <p>Saatmine: {{ shipping }}€</p>
+        <!-- Coding challenge'is lingi lisamine tootele -->
+        <a :href="url" target="_blank">Vaata videot sokkidest</a>
+        <!-- tooteinfo lisamine -->
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <!-- Coding challenge'ist, ise ei leidnud töötavat lahendust -->
+        <ul>
+          <li v-for="(size, index) in sizes" :key="index">{{ size }}</li>
+        </ul>
+        <!-- värvide lisamine -->
+        <div v-for="(variant, index) in variants" :key="variant.id" @mouseover="updateVariant(index)" class="color-circle" :style="{ backgroundColor: variant.color }"></div>
+        <!-- Lisa ostukorvi ja eemalda ostukorvist nupud-->
+        <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" v-on:click="addToCart">Lisa ostukorvi</button>
+        <button class="button" v-on:click="removeFromCart">Eemalda ostukorvist</button>
+      </div>
+    </div>
+  </div>`,
+
+  data() {
+    return {
+        // toote nimi
+        product: 'Sokid',
+        // bränd
+        brand: 'Vue Mastery',
+        // eri variantide koguste jaoks
+        selectedVariant: 0,
+        // video
+        url: 'https://www.youtube.com/watch?v=8JVpRtngAJU',
+        // toote detailid
+        details: ['50% puuvill', '30% vill', '20% polüester'],
+        // suuruste variandid
+        sizes: ['36-38', '39-41', '42-44'],
+        // toote variandid
+        variants: [
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+            { id: 2235, color: 'darkblue', image: './assets/images/socks_blue.jpg', quantity: 0},
+        ],
+        // challenge'i lahedus, ei mõelnud ise välja
+        onSale: true,
+    }
+},
+methods: {
+        addToCart() {
+            this.cart += 1
+    },
+    // ise ei osanud kirjutada seda miinusesse minemise piirajat
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -=1
+            }
+    },
+        updateVariant(index) {
+            this.selectedVariant = index
+        },
+},
+computed: {
+    title() {
+        return this.brand + ' ' + this.product
+    },
+    image() {
+        return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+        return this.variants[this.selectedVariant].quantity
+    },
+    // codechallenge'i lahendus, ei mõelnud ise välja
+    saleMessage() {
+        if (this.onSale) {
+            return this.brand + ' ' + this.product + ' on müügil'
+        }
+        return ''
+    },
+    shipping() {
+        if (this.premium) {
+            return 'tasuta'
+        }
+        return 2.99
+    }
+}
+})
--- main.js
+++ main.js
@@ -2,57 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            // toote nimi
-            product: 'Sokid',
-            // bränd
-            brand: 'Vue Mastery',
-            // eri variantide koguste jaoks
-            selectedVariant: 0,
-            // video
-            url: 'https://www.youtube.com/watch?v=8JVpRtngAJU',
-            // toote detailid
-            details: ['50% puuvill', '30% vill', '20% polüester'],
-            // suuruste variandid
-            sizes: ['36-38', '39-41', '42-44'],
-            // toote variandid
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
-                { id: 2235, color: 'darkblue', image: './assets/images/socks_blue.jpg', quantity: 0},
-            ],
-            // challenge'i lahedus, ei mõelnud ise välja
-            onSale: true,
+            premium: false,
         }
     },
-    methods: {
-            addToCart() {
-                this.cart += 1
-        },
-        // ise ei osanud kirjutada seda miinusesse minemise piirajat
-            removeFromCart() {
-                if (this.cart >= 1) {
-                    this.cart -=1
-                }
-        },
-            updateVariant(index) {
-                this.selectedVariant = index
-            },
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        // codechallenge'i lahendus, ei mõelnud ise välja
-        saleMessage() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' on müügil.'
-            }
-            return ''
-        }
-    }
+    methods: {},
+    
 })
